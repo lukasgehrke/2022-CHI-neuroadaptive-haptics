9ed107cee6c05542069c7754260c2a5d04964b99
aleksandrskoselevs <aleksandrs.koselevs.business@gmail.com>
2023-09-01 15:33:37 +0200

    introduced correct_state

--- ucbq_environment.py
+++ ucbq_environment.py
@@ -3,13 +3,13 @@ import random
 import numpy as np
 
 class ModifiedRandomEnvironment:
-    def __init__(self, current_state = 2, num_states=10):
+    def __init__(self, correct_action = 6, num_states=10):
         # The total number of feedback levels
         self.num_states = num_states
         # The last feedback level sent
         self.current_state = np.random.randint(num_states)
         # The "right" level of feedback
-        self.internal_state = 2
+        self.correct_action = correct_action
 
     def get_participant_answer(self, action):
         # TODO: use this code when we'll be listenting to the 
@@ -25,8 +25,12 @@ class ModifiedRandomEnvironment:
         # Wait random time before giving an answer.
         # This simulates listening to stream.
         time.sleep(random.uniform(0.001, 0.002))
-        # The reward is 0 if action matches current state, otherwise -1
-        answer = 0 if action == 2 else -1 
+
+        # # The reward is 0 if action matches current state, otherwise -1
+        # answer = 0 if action == 2 else -1 
+
+        answer = 0 if action == self.correct_action else -1 
+
         return answer
 
     def step(self, action):
