98db09d2d5a33bff17987a0e093723dddfb8798c
aleksandrskoselevs <aleksandrs.koselevs.business@gmail.com>
2023-09-23 12:24:38 +0200

    fixed max episode times because of convergence

--- neuro_haptics/aleks/ucbq_environment_stateless.py
+++ neuro_haptics/aleks/ucbq_environment_stateless.py
@@ -12,10 +12,13 @@ class ModifiedRandomEnvironment(ModifiedRandomEnvironment):
         self.same_action = None
         self.t = 0
         self.consecutive_limit = 15
+        self.consecutive_count = 0
 
     def step(self, action):
         self.t += 1
+
         reward = self.get_participant_answer(action)
+        # TODO: delete
         # Our case action == state, but migth consdier this separation in the future
         next_state = 0
         self.current_state = next_state
@@ -23,12 +26,12 @@ class ModifiedRandomEnvironment(ModifiedRandomEnvironment):
         done = False
         if self.t > 35:
             if action == self.same_action:
-                consecutive_count += 1
-                if consecutive_count >= self.consecutive_limit:
+                self.consecutive_count += 1
+                if self.consecutive_count >= self.consecutive_limit:
                     # tqdm.write(f"Selected action {action} - breaking loop at iteration {t} - consecutive count: {consecutive_count}")
                     done = True
             else:
-                same_action = action
-                consecutive_count = 1
+                self.same_action = action
+                self.consecutive_count = 1
         
         return reward, next_state, done
