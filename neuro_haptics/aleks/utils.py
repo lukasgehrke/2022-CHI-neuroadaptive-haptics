6a2ae60ea0f8444dbc92220670565361cad1f0ac
aleksandrskoselevs <aleksandrs.koselevs.business@gmail.com>
2023-09-26 17:32:53 +0200

    fix, but alpha decay still bad

--- neuro_haptics/aleks/utils.py
+++ neuro_haptics/aleks/utils.py
@@ -5,4 +5,5 @@ def print_agent_stats(agent):
     print(f'{np.around(agent.Q, decimals=4)}')
     print(f'Number of times action was taken:')
     print(f'{agent.N}')
-    print(f'Total timesteps: {sum(sum(agent.N)) - 100}')
+    print(f'Total timesteps:')
+    print(agent.t)
