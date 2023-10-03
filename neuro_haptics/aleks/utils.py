7d5054b8c01e2af116cb4f31059fb1c7976f073d
aleksandrskoselevs <aleksandrs.koselevs.business@gmail.com>
2023-10-03 18:16:23 +0200

    cleaned up params

--- neuro_haptics/aleks/utils.py
+++ neuro_haptics/aleks/utils.py
@@ -10,3 +10,17 @@ def print_agent_stats(agent):
 
 def get_num_unique_rewards(num_actions, correct_action):
     return max(abs(num_actions - correct_action), abs(correct_action + 1))
+
+def get_mean_across_episodes(arr):
+    min_cols = np.amin([len(row) for row in arr])
+    truncated_arr = [ x[:min_cols] for x in arr ]
+    res = np.array(truncated_arr)
+    mean = res.mean(axis=0)
+
+    return mean    
+
+def get_cumsum_rewards(rewards):
+    cumulative_sum_rewards = np.cumsum(rewards)
+    time_steps = np.arange(1, len(rewards) + 1)
+    mean_rewards = cumulative_sum_rewards / time_steps
+    return mean_rewards
