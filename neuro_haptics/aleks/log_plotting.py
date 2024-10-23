3a804a520e698ea093e40b7aadfd9526c2bb7bd1
aleksandrskoselevs <aleksandrs.koselevs.business@gmail.com>
2024-10-23 15:26:46 +0200

    Refactor log_plotting.py for improved readability and customization

--- neuro_haptics/aleks/log_plotting.py
+++ neuro_haptics/aleks/log_plotting.py
@@ -6,54 +6,49 @@ colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:5]
 # Create a map that maps integers to these colors
 color_map = {i: color for i, color in enumerate(colors)}
 
-color_map
-
-def plot_q_learning(df, fig, ax):
-    initial_q_value = 0
-    
-    df_plot_q_values = df.pivot(index='t', columns='action', values='new_Q_value')
-    
-    # Draw dots if the action was chosen
-    # Need the NaNs here
+def plot_q_values(df_plot_q_values, ax, initial_q_value=0):
     line_colors = [color_map[col] for col in df_plot_q_values.columns]
-    df_plot_q_values.plot(ax=ax, marker='o', markersize=8, zorder=3, color=line_colors)
-
+    df_plot_q_values.plot(ax=ax, marker='o', 
+                        #   markersize=8, zorder=3, 
+                          color=line_colors)
     
-    # Draw lines connecting the dots, representing current Q values
-    df_plot_q_values = df_plot_q_values.interpolate()
-    df_plot_q_values = df_plot_q_values.fillna(initial_q_value)    
-
+    df_plot_q_values = df_plot_q_values.interpolate().fillna(initial_q_value)
     for col in range(5):
         if col not in df_plot_q_values.columns:
             df_plot_q_values[col] = initial_q_value
+    df_plot_q_values.plot(ax=ax, 
+                        #   linewidth=3, 
+                          color=line_colors)
 
-    df_plot_q_values.plot(ax=ax, linewidth=3, color=line_colors)
+def plot_additional_metrics(df, ax):
+    df.plot('t', ['reward_adjusted', 'alpha', 'epsilon'],
+            # Re-enable when plotting not_separate
+            #  color=['lightblue', 'lightgreen', 'lightpink'], 
+             ax=ax, zorder=0, linewidth=1)
 
-
-
-
-    df_plot = df
-
-    df_plot.plot('t', [
-        # 'reward', 
-        'reward_adjusted', 
-        'alpha', 'epsilon'], color=['lightblue', 'lightgreen', 'lightpink'],
-        ax=ax, zorder=0,
-        linewidth=1)
-
-    # lines = ax.get_lines()
-    # lines[0].set_linewidth(2)  # Make the reward line thicker
-    # lines[1].set_color('lightblue')  # Make the reward_adjusted line light blue
-    # lines[0].set_linestyle('--')  # Make the reward line dashed
-
-    # Draw a vertical light gray line at x=25
-    ax.axvline(x=25, color='lightgray', linestyle='--', zorder=0, linewidth=1)
-
-    # Make the plot bigger
-    fig = plt.gcf()
-    fig.set_size_inches(12, 8)
-
-
-    # Exclude line labels
+def customize_plot(ax):
+    ax.axvline(x=25, color='lightgray', linestyle='--', 
+            #    zorder=0, linewidth=1
+               )
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles[:5] + handles[10:], labels[:5] + labels[10:], fontsize='small')
+
+def plot_q_learning(df, ax):
+    df_plot_q_values = df.pivot(index='t', columns='action', values='new_Q_value')
+    plot_q_values(df_plot_q_values, ax)
+    plot_additional_metrics(df, ax)
+    customize_plot(ax)
+
+def plot_q_learning_separate(df, ax):
+    initial_q_value = 0
+    ax1, ax2 = ax
+    
+    df_plot_q_values = df.pivot(index='t', columns='action', values='new_Q_value')
+    plot_q_values(df_plot_q_values, ax1, initial_q_value)
+    plot_additional_metrics(df, ax2)
+    
+    ax1.axvline(x=25, color='lightgray', linestyle='--', zorder=0)
+    ax2.axvline(x=25, color='lightgray', linestyle='--', zorder=0)
+       
+    handles1, labels1 = ax1.get_legend_handles_labels()
+    ax1.legend(handles1[:5], labels1[:5], fontsize='small')
