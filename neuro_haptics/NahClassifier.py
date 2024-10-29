c687184d336f688ed390b148a601f7e44de5bc17
Lukas Gehrke <lukas.gehrke@tu-berlin.de>
2024-10-29 11:06:45 +0100

    Refactor NahClassifier.py for improved data retrieval and prediction accuracy

--- neuro_haptics/NahClassifier.py
+++ neuro_haptics/NahClassifier.py
@@ -11,10 +11,8 @@ import concurrent.futures
 class NahClassifier:
     def __init__(self, model_path):
 
-        # Initialize the model and imputer
-        self.model = LinearDiscriminantAnalysis()
-        self.imputer = SimpleImputer(strategy='mean')
-        self.expected_num_features = 521  # Set this to the number of features the model expects
+        # self.imputer = SimpleImputer(strategy='mean')
+        # self.expected_num_features = 521  # Set this to the number of features the model expects
         
         # Load the pre-trained model
         self.model = pickle.load(open(model_path, 'rb'))
@@ -25,6 +23,7 @@ class NahClassifier:
         with open(model_path.replace('model', 'bci_params').replace('.sav', '.json')) as f:
             bci_params = json.load(f)
         self.target_class = bci_params['target_class']
+        self.mean_fix_delay = bci_params['mean_fix_delay']
     
         # resolve streams
         streams = None
@@ -74,17 +73,15 @@ class NahClassifier:
         # self.mne_raw_info = mne.create_info(ch_names=[f"EEG{n:01}" for n in range(1, 66)],  ch_types=["eeg"] * 65, sfreq=self.srate)
     
     def predict(self, features):
-        # Impute missing values
-        features = self.imputer.fit_transform(features)
 
-        # Adjust the number of features to match the model's expected input shape
-        if features.shape[1] < self.expected_num_features:
-            # Pad with zeros if there are fewer features
-            padding = np.zeros((features.shape[0], self.expected_num_features - features.shape[1]))
-            features = np.hstack((features, padding))
-        elif features.shape[1] > self.expected_num_features:
-            # Truncate if there are more features
-            features = features[:, :self.expected_num_features]
+        # # Adjust the number of features to match the model's expected input shape
+        # if features.shape[1] < self.expected_num_features:
+        #     # Pad with zeros if there are fewer features
+        #     padding = np.zeros((features.shape[0], self.expected_num_features - features.shape[1]))
+        #     features = np.hstack((features, padding))
+        # elif features.shape[1] > self.expected_num_features:
+        #     # Truncate if there are more features
+        #     features = features[:, :self.expected_num_features]
 
         # Predict the class
         prediction = int(self.model.predict(features)[0])  # predicted class
@@ -94,9 +91,7 @@ class NahClassifier:
 
         return prediction, probs_target_class, score
         
-    def discretize(self, prediction, score):
-
-        # if prediction == self.target_class:
+    def discretize(self, score):
 
         # find the boundaries for the classifier
         for i, boundary in enumerate(self.boundaries):
@@ -138,6 +133,12 @@ class NahClassifier:
         elif type == 'eye':
             return all_eye_data.T
         elif type == 'marker':
+
+            if fix_delay == 0:
+                print("No fix delay detected, using mean fix delay")
+                # fix_delay = np.nan
+                fix_delay = self.mean_fix_delay
+
             return fix_delay
 
     def compute_features(self, data, modality):
@@ -153,78 +154,45 @@ class NahClassifier:
             windowed_means = windowed_mean(data, srate, window_size)
             
             # select features of interest
-            windowed_mean = windowed_mean[:,1:9].flatten()
+            windowed_means = windowed_means[:,1:9].flatten()
 
-            return windowed_mean
+            return windowed_means
 
         # eye processing and feature extraction
         elif modality == 'eye':
+
+            srate = data.shape[1]
 
             # channels for eye data
             gaze_direction_chans = np.arange(2,5)
             gaze_validity_chan = 10
 
             # Use the function in the compute_features method
-            gaze_velocity = compute_gaze_velocity(data, gaze_direction_chans, gaze_validity_chan)
+            gaze_velocity = compute_gaze_velocity(data, srate, window_size, gaze_direction_chans, gaze_validity_chan)
+            gaze_velocity = np.expand_dims(gaze_velocity, axis=0)
+            windowed_means = windowed_mean(gaze_velocity, srate, window_size)
+            windowed_means = windowed_means[:,1:9].flatten()
 
-            # Example: Compute windowed means
-            gaze_velocity = gaze_velocity[baseline_end:]
-
-            # Example values for num_windows and window_size
-            num_windows = 8
-            window_size = 12
-
-            # Compute the required size
-            required_size = num_windows * window_size
-
-            # Print the current size of gaze_velocity
-            print(f"Size of gaze_velocity: {gaze_velocity.size}")
-
-            # Pad gaze_velocity if its size is less than the required size
-            if gaze_velocity.size < required_size:
-                padding_size = required_size - gaze_velocity.size
-                gaze_velocity = np.pad(gaze_velocity, (0, padding_size), 'constant', constant_values=np.nan)
-                print(f"Padded gaze_velocity to size: {gaze_velocity.size}")
-
-            # Truncate gaze_velocity if its size is greater than the required size
-            elif gaze_velocity.size > required_size:
-                gaze_velocity = gaze_velocity[:required_size]
-                print(f"Truncated gaze_velocity to size: {gaze_velocity.size}")
-
-            # Reshape gaze_velocity
-            reshaped_gaze = gaze_velocity.reshape(num_windows, window_size)
-            print(f"Reshaped gaze_velocity to shape: {reshaped_gaze.shape}")
-
-            # Compute gaze features
-            gaze_features = reshaped_gaze.mean(axis=1).flatten()
-            print(f"Gaze features: {gaze_features}")
-
-            return gaze_features
+            return windowed_means
 
     def choose_nah_label(self):
 
         # this needs to pull data directly after the grab marker
-        # needs to pull exactly 108 samples for eeg
-
         tic = time.time()
         with concurrent.futures.ThreadPoolExecutor() as executor:
             eeg = executor.submit(self.get_data, 'eeg')
             eye = executor.submit(self.get_data, 'eye')
-            fix_delay = self.get_data('marker')
-        print(f"EEG data shape: {eeg.result().shape}, Eye data shape: {eye.result().shape}")
+            fix_delay = executor.submit(self.get_data, 'marker')
         toc = time.time()
         print(toc - tic)
 
         # compute features and predict    
-        eeg_feat = classifier.compute_features(eeg, 'eeg') # this needs to pull data directly after the grab marker
-        eye_feat = classifier.compute_features(eye, 'eye') # this needs to pull data directly after the grab marker
+        eeg_feat = classifier.compute_features(eeg.result(), 'eeg') # this needs to pull data directly after the grab marker
+        eye_feat = classifier.compute_features(eye.result(), 'eye') # this needs to pull data directly after the grab marker
 
-        feature_vector = np.concatenate((eeg_feat, eye_feat, [fix_delay]), axis=0).reshape(1, -1)
-
-        # Predict the class
+        feature_vector = np.concatenate((eeg_feat, eye_feat, [fix_delay.result()]), axis=0).reshape(1, -1)
         prediction, probs_target_class, score = classifier.predict(feature_vector)
-
-        discrete_prediction = self.discretize(prediction, score)
+        discrete_prediction = self.discretize(score)
 
         return discrete_prediction
     
@@ -237,7 +205,6 @@ class NahClassifier:
         # to only listen to the prediction at a certain time?
         prediction = str(prediction)
         self.labels.push_sample(prediction)
-
 
 # main
 if __name__ == "__main__":
@@ -252,64 +219,45 @@ if __name__ == "__main__":
     
     classifier = NahClassifier(model_path)
 
-    while True:
+    time.sleep(4) # wait for the streams buffers to fill up
 
-        tic = time.time()
-        choose_label = classifier.choose_nah_label()
-        print(choose_label)
-        print(f"Time to choose label: {time.time() - tic}")
-
-    # TODO clean this up? but the -1 here had some logic, ask aleks
-    # last_grab_number = -1
+    last_grab_number = -1
 
     # start_print_time = time.time()
     # last_print_time = start_print_time
     
-    # while True:
-    #     current_time = time.time()
-    #     if current_time - last_print_time >= 1:
-    #         elapsed_time = current_time - start_print_time
-    #         minutes, seconds = divmod(int(elapsed_time), 60)
-    #         formatted_time = f"{minutes:02}:{seconds:02}"
-    #         print(f"{formatted_time} - Waiting for grab marker...")
-    #         last_print_time = current_time
+    while True:
+        # current_time = time.time()
+        # if current_time - last_print_time >= 1:
+        #     elapsed_time = current_time - start_print_time
+        #     minutes, seconds = divmod(int(elapsed_time), 60)
+        #     formatted_time = f"{minutes:02}:{seconds:02}"
+        #     print(f"{formatted_time} - Waiting for grab marker...")
+        #     last_print_time = current_time
 
-    #     marker = classifier.marker_inlet.pull_sample()[0]
+        marker = classifier.marker_inlet.pull_sample()[0]
+        print(marker)
 
-    #     # what if there are two grab markers
-    #     if marker and 'What:' in marker[0]:
-    #         marker_data = marker[0].split(';')
-    #         marker_dict = {item.split(':')[0]: item.split(':')[1] for item in marker_data}
-    #         what = marker_dict.get('What')
+        # this is a working test marker
+        # time.sleep(2)
+        # marker = ['What:grab;Number:1']
+
+        # what if there are two grab markers
+        if marker and 'What:' in marker[0]:
+            marker_data = marker[0].split(';')
+            marker_dict = {item.split(':')[0]: item.split(':')[1] for item in marker_data}
+            what = marker_dict.get('What')
             
-    #         if what == 'grab':
-    #             current_grab_number = int(marker_dict.get('Number', 0))
+            if what == 'grab':
+                current_grab_number = int(marker_dict.get('Number', 0))
 
-    #             if current_grab_number > last_grab_number:           
-    #                 print(f"Grab {current_grab_number} detected: ", marker)
+                if current_grab_number > last_grab_number:           
+                    print(f"Grab {current_grab_number} detected: ", marker)
 
-    #                 eeg, eye, fix_delay = classifier.get_data()
+                    tic = time.time()
+                    mapped_label = classifier.choose_nah_label()
+                    print(f"Time to choose label: {time.time() - tic}")
+
+                    classifier.send_nah_label_to_ai(mapped_label)
                     
-    #                 eeg_feat = classifier.compute_features(eeg, 'eeg')
-    #                 eye_feat = classifier.compute_features(eye, 'eye')
-
-    #                 # for tests
-    #                 # eeg = classifier.get_data()
-    #                 # eye = classifier.get_data()            
-    #                 # eye_feat = np.zeros(8)
-    #                 # test_fix_delay = np.array([0.4])
-
-    #                 # concatenate eeg, eye, fix_delay
-    #                 feature_vector = np.concatenate((eeg_feat, eye_feat, [fix_delay]), axis=0).reshape(1, -1)
-
-    #                 # pred
-    #                 prediction, probs_target_class, score = classifier.predict(feature_vector)
-
-    #                 print(f'Classifier stuff {prediction}, {probs_target_class}, {score}')
-                    
-    #                 # Map probs_target_class to an int value in the range 1 to 5
-    #                 mapped_label = max(1, int(np.ceil(probs_target_class * 5)))
-
-    #                 classifier.send_nah_label_to_ai(mapped_label)
-                    
-    #                 print("Label sent to AI: ", mapped_label)
+                    print("Label sent to AI: ", mapped_label)
